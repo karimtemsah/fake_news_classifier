b13414c1ceedb0f3c52a41140da475e8d94a6454
Karim Temsah <>
2017-12-14 17:33:28 +0100

    adding the stream results to test files

--- stream.py
+++ stream.py
@@ -2,8 +2,11 @@ import time
 import json
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
+import pickle
+import pandas as pd
 
-
+model = pickle.load(open('models/naiiveBayes.pkl', 'rb'))
+count_vectorizer = pickle.load(open('models/count_vector.pkl', 'rb'))
 def filter_tweets(tweet):
     json_tweet = json.loads(tweet)
     if 'lang' in json_tweet.keys():  # When the lang key was not present it caused issues
@@ -15,11 +18,13 @@ def filter_tweets(tweet):
 def map_tweets(tweet):
     json_tweet = json.loads(tweet)
     if 'text' in json_tweet.keys():
-        return json_tweet['text']
+        current_tweet = json_tweet['text']
+        #print(model.predict(count_vectorizer.transform(current_tweet)))
+        return current_tweet, model.predict(count_vectorizer.transform(pd.Series(current_tweet)))
     return None
 
 
-sc = SparkContext("local[2]", "Simple App")
+sc = SparkContext("local[3]", "Simple App")
 ssc = StreamingContext(sc, 10)
 lines = ssc.socketTextStream("127.0.0.1", 5555)
 #lines.foreachRDD(lambda rdd: rdd.filter(filter_tweets).coalesce(1).saveAsTextFile("./tweets/%f" % time.time()))
